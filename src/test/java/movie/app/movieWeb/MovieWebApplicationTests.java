95d24191dbc73b1e8c07de8992e99a9276bbae16
eriks.kazus@esynergy.lv <eriks.kazus@esynergy.lv>
2024-02-05 10:36:07 +0200

    movePlace_back

---
+++ src/test/java/movie/app/movieWeb/MovieWebApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.app.movieWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
